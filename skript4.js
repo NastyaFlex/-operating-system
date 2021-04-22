ebdd14c7e573d87f08a623eb192dac5451f2c139
NastyaFlex <74597359+NastyaFlex@users.noreply.github.com>
2021-04-22 15:17:30 +0500

    test

--- skript4.js
+++ skript4.js
@@ -1,31 +1,27 @@
-let reaT;
 let papaya = new XMLHttpRequest();
-let namesVM = document.getElementById('NamesVM')
-papaya.open('GET',`http://10.3.0.81:10005/list_vm?token=${localStorage.getItem("chef")}`);
+papaya.open('GET',`http://10.3.0.81:10005/listVMs?token=${localStorage.getItem("chef")}`);
 papaya.send();//отправляем запрос на сервер
-// papaya.onload = function(){
-//   if (papaya.status === 200) { //если папайя равна 200, то оно работает, ВООООТ
-//     reaT = JSON.parse(papaya.response);//переход текста в объект
-//     // console.log(reaT);
-//     //
-//     // console.log(reaT.params);
-//     for (var i = 0; i < reaT.length; i++) {
-//       namesVM.insertAdjacentHTML('beforeend',`
-//       <h1 onclick = 'testForTest(${i})' class = 'dinamicNameVm'>${reaT[i].params.nameVM}</h1>
-//       `);
-//     }
-//   }
-// }
-//
-//
-// function testForTest (i) {
-//   console.log(reaT[i]);
-//   if (!document.getElementById(i)) {
-//     namesVM.insertAdjacentHTML('beforeend',`
-//     <p id = '${i}' onclick = 'testForTest(${i})' class = 'dinamicNameVm'> ${reaT[i].port} </p>
-//     `);
-//   }
-// }
+
+// Этот код сработает после того, как мы получим ответ сервера
+papaya.onload = function(){
+  if(papaya.status != 200){
+    console.log(papaya.status + " Ошибка");
+  } else{
+    let jsontext = papaya.response;
+    console.log(jsontext);
+    let arrVMs = JSON.parse(jsontext);
+    console.log(arrVMs);
+
+    let car = document.getElementById("car");
+    console.log(car);
+
+     for(let i in arrVMs){
+       console.log(i);
+     }
+
+  }
+}
+
 function klik(){
     document.location.href = "index3.html";
 }
